bc7df96755d90a2c72a3183b4c57c8da21503f18
Mohamed Shazan <msmshazan1@gmail.com>
2019-08-01 22:37:16 +0530

    Remove unwanted code

--- ImFormsCallSiteWeaver/ImFormsCallsiteWeaver.cs
+++ ImFormsCallSiteWeaver/ImFormsCallsiteWeaver.cs
@@ -60,15 +60,6 @@ namespace Weavers
                     method.Body.OptimizeMacros();
                 }
             }
-            var allmethodnames = string.Join(" , ", calledmethods);
-            var ns = GetNamespace();
-            var type = new TypeDefinition(ns, "Hello", TypeAttributes.Public, TypeSystem.ObjectReference);
-
-            AddConstructor(type);
-
-            AddHelloWorld(type, allmethodnames);
-
-            ModuleDefinition.Types.Add(type);
 
         }
 
@@ -81,81 +72,7 @@ namespace Weavers
             yield return "netstandard";
         }
 
-        string GetNamespace()
-        {
-            var namespaceFromConfig = GetNamespaceFromConfig();
-            var namespaceFromAttribute = GetNamespaceFromAttribute();
-            if (namespaceFromConfig != null && namespaceFromAttribute != null)
-            {
-                throw new WeavingException("Configuring namespace from both Config and Attribute is not supported.");
-            }
-
-            if (namespaceFromAttribute != null)
-            {
-                return namespaceFromAttribute;
-            }
-
-            return namespaceFromConfig;
-        }
-
-        string GetNamespaceFromConfig()
-        {
-            var attribute = Config?.Attribute("Namespace");
-            if (attribute == null)
-            {
-                return null;
-            }
-
-            var value = attribute.Value;
-            ValidateNamespace(value);
-            return value;
-        }
-
-        string GetNamespaceFromAttribute()
-        {
-            var attributes = ModuleDefinition.Assembly.CustomAttributes;
-            var namespaceAttribute = attributes
-                .SingleOrDefault(x => x.AttributeType.FullName == "NamespaceAttribute");
-            if (namespaceAttribute == null)
-            {
-                return null;
-            }
-
-            attributes.Remove(namespaceAttribute);
-            var value = (string)namespaceAttribute.ConstructorArguments.First().Value;
-            ValidateNamespace(value);
-            return value;
-        }
-
-        static void ValidateNamespace(string value)
-        {
-            if (value is null || string.IsNullOrWhiteSpace(value))
-            {
-                throw new WeavingException("Invalid namespace");
-            }
-        }
-
-        void AddConstructor(TypeDefinition newType)
-        {
-            var attributes = MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName;
-            var method = new MethodDefinition(".ctor", attributes, TypeSystem.VoidReference);
-            var objectConstructor = ModuleDefinition.ImportReference(TypeSystem.ObjectDefinition.GetConstructors().First());
-            var processor = method.Body.GetILProcessor();
-            processor.Emit(OpCodes.Ldarg_0);
-            processor.Emit(OpCodes.Call, objectConstructor);
-            processor.Emit(OpCodes.Ret);
-            newType.Methods.Add(method);
-        }
-
-        void AddHelloWorld(TypeDefinition newType, string str)
-        {
-            var method = new MethodDefinition("World", MethodAttributes.Public, TypeSystem.StringReference);
-            var processor = method.Body.GetILProcessor();
-            processor.Emit(OpCodes.Ldstr, str);
-            processor.Emit(OpCodes.Ret);
-            newType.Methods.Add(method);
-        }
-
+      
         public override bool ShouldCleanReference => true;
     }
 
